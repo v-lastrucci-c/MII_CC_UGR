3dba25f3f3baaa176f649384709a5acaeac3df19
v-lastrucci-c <v.lastrucci.c@gmail.com>
2024-11-11 21:32:21 +0100

    feat: closes #23; use conftest to have .fixtures

--- src/web/tests/test_flask_app.py
+++ src/web/tests/test_flask_app.py
@@ -1,51 +1,5 @@
-
-import os
-import pytest
-from app import create_app, db
 from app.models import User
 from flask import url_for
-
-# Configuración de prueba
-@pytest.fixture
-def app():
-    # Crear la aplicación en modo de prueba
-    os.environ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-        "SQLALCHEMY_DATABASE_URI": "sqlite:///:memory:",
-        "SECRET_KEY": "test",
-        "SERVER_NAME": "localhost",
-    })
-
-    # Crear la base de datos y el esquema
-    with app.app_context():
-        db.create_all()
-        yield app
-        db.session.remove()
-        db.drop_all()
-
-@pytest.fixture
-def client(app):
-    # Asegurar que el cliente se use dentro de un contexto de solicitud
-    with app.test_client() as client:
-        with app.app_context():
-            yield client
-
-@pytest.fixture
-def runner(app):
-    return app.test_cli_runner()
-
-# Fixture para crear un usuario de prueba
-@pytest.fixture
-def user(app):
-    with app.app_context():
-        user = User(name="testuser", email="test@example.com")
-        user.set_password("mysecretpassword")
-        db.session.add(user)
-        db.session.commit()
-        db.session.refresh(user)  # Asegurar que el objeto esté enlazado a la sesión
-        return user
 
 # Tests para el modelo User
 def test_user_creation(app, user):
