58c001e9d02d70a1caafc05dbf847f02522cf7ca
v-lastrucci-c <v.lastrucci.c@gmail.com>
2024-12-01 17:48:20 +0100

    feat: close #45; define Users model for api

---
+++ src/api/app/internal/models.py
@@ -0,0 +1,25 @@
+"""
+Contiene los modelos relacionados con
+ la base de datos.
+
+Uso:
+    ./models.py
+"""
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.orm import declarative_base
+from datetime import datetime, timezone
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    # Columnas de la tabla
+    email = Column(String(50), primary_key=True, nullable=False)  # Clave primaria
+    name = Column(String(25), nullable=False)  # Nombre del usuario
+    password_hash = Column(String, nullable=False)  # Hash de la contraseña
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))  # Fecha de creación
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))  # Fecha de actualización
+
+    def __repr__(self):
+        return f"<User email={self.email} name={self.name}>"
