892c6c83d1ace3514136f604064d89eca7610622
v-lastrucci-c <v.lastrucci.c@gmail.com>
2025-01-24 18:22:57 +0100

    feat: update apiBaseUrl name

--- src/web/app/auth.py
+++ src/web/app/auth.py
@@ -38,7 +38,7 @@ def login_required(view):
 @login_required
 def index():
     logger.info("Cargando página principal para usuario autenticado.")
-    return render_template('index.html', apiBaseUrl=os.getenv("apiBaseUrl"))
+    return render_template('index.html', apiBaseUrl=os.getenv("API_BASE_URL"))
 
 @bp.before_app_request
 def load_logged_in_user():
